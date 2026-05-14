758be9c315b14ff9c41e596a6a5c4221cbebb1fa
Mohin Pathan <mohinpathan2004@gmail.com>
2026-05-14 12:16:30 +0530

    added

---
+++ ch-2-Variables/5-datatype.js
@@ -0,0 +1,24 @@
+//there are two type of data type 
+//1.is Primitive datatype
+
+//1.Number
+
+let number=1234;
+console.log(number);
+console.log(typeof number);
+
+//2.String
+
+let name="Mohin";
+console.log(name)
+console.log(typeof name);
+
+//3.undefine
+let a;
+console.log(typeof a);
+
+//4.bollen
+
+let Person=true;
+console.log(Person);
+console.log(typeof Person);
