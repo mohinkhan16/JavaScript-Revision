5946e62ebde614dbc4642ea67eff3f11bfa4758f
Mohin Pathan <mohinpathan2004@gmail.com>
2026-05-20 08:48:37 +0530

    added

---
+++ ch-6-loops/forloop.js
@@ -0,0 +1,14 @@
+
+
+for(let i=0;i<=1000;i++){
+    console.log(i);
+}
+
+
+const fruites=[
+    "Mango","Avacodo","Cherry","Banana","Apple"
+]
+
+for(let i=0;i<fruites.length;i++){
+    console.log(fruites[i]);
+}
