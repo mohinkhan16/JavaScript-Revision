23ded23f55b4b3fadc7fd0ee5b754438910d7785
Mohin Pathan <mohinpathan2004@gmail.com>
2026-05-22 08:31:49 +0530

    added

---
+++ ch-9-Execution-contaxt/1.memory-code.js
@@ -0,0 +1,16 @@
+
+
+// first in whatever happend in js is for execution contaxt...
+
+var a= 4;
+
+function Squre(num){
+    var result= num*num;
+    return result;
+}
+
+var Squre2=Squre(a);
+var Squre4=Squre(4)
+
+console.log(Squre2)
+console.log(Squre4)
---
+++ ch-9-Execution-contaxt/2.Hoisted.js
@@ -0,0 +1,15 @@
+
+
+hoistedvariable ="Mohin"
+
+console.log(hoistedvariable);
+
+var hoistedvariable;
+
+
+
+//intislization are not hoisted but decleraction are hoisted..
+var x;
+console.log(x);
+
+x=40;
