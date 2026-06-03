cea653beafb70b3450f3f52f69bd592dbc216547
Mohin Pathan <mohinpathan2004@gmail.com>
2026-06-03 08:26:17 +0530

    Array code added

---
+++ ch_12_Array/1.Array.js
@@ -0,0 +1,25 @@
+
+//creating an array using array liiteral
+
+
+const Fruites=["Mango","Banana","Apple"];
+
+console.log(Fruites);
+
+
+const bikes=["Ktm","Splender","E-bike"];
+
+console.log(bikes);
+
+
+
+//using array constructor
+
+const veges= new Array("Tamoto","Poteto","Chill","Garlic");
+
+console.log(veges);
+
+
+const MovieName = new Array("Ishq","DDLJ","DhurnDhar");
+
+console.log(MovieName)
---
+++ ch_12_Array/2.Array.js
@@ -0,0 +1,29 @@
+
+
+let  Animal =[
+    "Monkey",
+    "Panda",
+    "Donkey",
+    "Cat",
+    "Goat"
+];
+
+
+//Access first element.
+
+console.log(Animal[0]);
+
+//Access Random Element
+
+console.log(Animal[2]);
+
+//Accesss Last Element
+
+console.log(Animal[Animal.length-1]);
+
+//Modification
+
+console.log(Animal[2]);
+Animal[2]="Lion";
+
+console.log(Animal[2]);
