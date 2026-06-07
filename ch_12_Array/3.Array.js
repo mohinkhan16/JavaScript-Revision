8788a34af9b76881c862f4214b4d99ad5a84d24c
Mohin Pathan <mohinpathan2004@gmail.com>
2026-06-07 08:36:20 +0530

    Array method push pop added

---
+++ ch_12_Array/3.Array.js
@@ -0,0 +1,22 @@
+//push
+
+let bikes=["BMW","Splender","Suzuki"];
+bikes.push("Activa");
+console.log(bikes);
+
+//unshift
+
+let fruites=["Banana","Mango","Orange"];
+fruites.unshift("Cherry");
+console.log(fruites);
+
+//pop
+let veges =["Domestick","Poteto","onine","garlic"];
+veges.pop();
+console.log(veges);
+
+//shift
+
+let details =["ID","Number","Address","G-mail"];
+details.shift();
+console.log(details);
