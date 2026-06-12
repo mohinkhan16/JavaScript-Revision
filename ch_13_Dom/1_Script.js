d67b4323dd048bc8ca1051ba8509f8119fd97ead
Mohin Pathan <mohinpathan2004@gmail.com>
2026-06-12 08:47:41 +0530

    queryselectore queryselectorealll

--- ch_13_Dom/1_Script.js
+++ ch_13_Dom/1_Script.js
@@ -1,30 +1,46 @@
-
-//id
-
-
 let heading = document.getElementById("Heading");
 
-heading.textContent="this is heading"
-
-console.log(heading);
-
-//by class
-
-const para =document.getElementsByClassName("para");
-console.log(para);
-
-para[0].textContent="This is class";
+heading.textContent = "this is heading";
 
 
-para[1].textContent="other paragraph";
+//by class name
+const para = document.getElementsByClassName("para");
 
-console.log(Para);
+para[0].textContent = "This is class";
+para[1].textContent = "other paragraph";
 
-//by tag name
 
+//tag
 let h5 = document.getElementsByTagName("h5");
-
-console.log(h5);
-
 h5[0].textContent = "Changed by JavaScript";
 
+//by query
+let title = document.querySelector(".title");
+title.textContent = "Changing Heading";
+
+//QueryselectorAll
+let titles = document.querySelectorAll(".title");
+
+titles.forEach((item) => {
+    item.textContent = "Changed";
+});
+
+//btn
+
+let btn=document.getElementById("click");
+
+btn.addEventListener("click", () => {
+    btn.textContent="Button click succesfully "
+});
+
+//form
+
+let form =document.getElementById("form");
+
+form.addEventListener("submit",(e)=>{
+    event.preventDefault();
+
+    let name=document.getElementById("name").value;
+
+    document.getElementById("result").textContent="form submitted successfully";
+});
