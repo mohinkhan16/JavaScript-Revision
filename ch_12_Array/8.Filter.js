ff641a034dcd6c7c86ca83b0559580b50f5d6970
Mohin Pathan <mohinpathan2004@gmail.com>
2026-06-14 20:59:49 +0530

    filter reduce

--- ch_12_Array/8.Filter.js
+++ ch_12_Array/8.Filter.js
@@ -9,3 +9,17 @@ const evenNumber =number.filter((number)=> number %2===0);
 
 console.log(evenNumber);
 
+
+const Person=[
+    {name :"Smarth",age:21,city:"vadodra",profession:"Doctor"},
+    {name :"Priya",age:23,city:"Ahemdabad",profession:"Teacher"},
+    {name :"Yash",age:27,city:"Baroda",profession:"Hair Stylest"},
+    {name :"Dhyey",age:31,city:"Mumbai",profession:"Graphic desiner"},
+    {name :"Jay",age:34,city:"Pune  ",profession:"Bussiness men"},
+
+];
+
+
+const people= Person.filter((x)=>x.age >20);
+
+console.log(people);
--- ch_12_Array/9.Reduce.js
+++ ch_12_Array/9.Reduce.js
@@ -8,3 +8,19 @@ const Number=numbers.reduce((acc,cuu)=>{
 },)
 
 console.log(Number)
+
+
+const productDetails = [
+  { productName: "Laptop", price: 1200,  stock: 15 },
+  { productName: "Office Chair", price: 250,  stock: 30 },
+  { productName: "Smartphone", price: 800,  stock: 25 },
+  { productName: "Desk Lamp", price: 45,  stock: 50 },
+  { productName: "Backpack", price: 60,  stock: 40 },
+];
+
+
+const total=productDetails.reduce((acc,cur)=>{
+    return (acc=acc+cur.price);
+},0);
+
+console.log(total);
