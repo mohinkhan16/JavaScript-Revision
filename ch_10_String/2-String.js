4ae8e48163943c155750a451346d70c42e072f27
Mohin Pathan <mohinpathan2004@gmail.com>
2026-07-24 12:25:54 +0530

    String method revision

---
+++ ch_10_String/2-String.js
@@ -0,0 +1,188 @@
+
+// 1:-  Match method
+  //Match method is use for give us to full details and
+  //   //  also give index if is it no working then he give us to Null output.
+
+  let play="i am playing cricket";
+  console.log("Match method:-",play.match("play"));
+
+
+// 2:-repeat
+//make a new string and copy strings
+
+const name="Mohin";
+console.log("Repeat :",Name.repeat(3));
+
+// 3:-replace
+// first match are replace
+// searching a new string a pattern and return a new string
+
+let latter="are you okay";
+console.log("Replace :",latter.replace("a","A"));
+
+// 4:-replace all
+// replace all work for replace all same latter 
+ word="this is book";
+ console.log("Replace All:-",word.replaceAll("o","O"));
+
+ //5:-search
+//  search a value and return a index position
+// search has a include a regular  expression 
+// index has a not a include a regular expression that the different between this two 
+
+let book="War and Peace";
+console.log("Search:-",book.search("a"));
+
+// 6:-slice 
+//abstract a part of string and return a new string
+
+let Animal="This is so many animal";
+console.log("Slice:-",Animal.slice(2));
+console.log("Slice:-",Animal.slice(6));
+console.log("Slice:-",Animal.slice(-1));
+
+// 7:-split 
+//firstly they convert string into array
+
+let mobile="This is my new mobile";
+console.log("Split:-",mobile.split(" "))   //with space
+console.log("Split:-",mobile.split(""))   //without space
+console.log("Split:-",mobile.split("i"))   //removing latter
+
+// 8:-start with
+// give us to output in boolean value 
+// if is latter in the string then output is true rather then output is false
+
+let text="hello everyone";
+
+console.log("Start With:-",text.startsWith("h"));
+
+// 9:-substring
+//substring does not change original array
+//
+
+let number="abcdefg";
+console.log("Substring:-",number.substring(2,5));
+console.log("Substring:-",number.substring(5,2));
+console.log("Substring:-",number.substring(-1));//give full text
+
+// 10:-uppercase
+//write touppercase()
+//if text has a lowercase we can change into uppercase
+
+let upper="good morning";
+console.log("Uppercase:-",upper.toUpperCase());
+
+// 11:-lowercase
+//write tolowercase()
+//if text has a uppercase wa can change into lowercase
+
+let lower="GOOD AFTERNOON";
+console.log("Lowercase:-",lower.toLowerCase());
+
+// 12:-trim 
+// use for remove white space
+
+let remove="  hello  everyone   ";
+
+console.log("Trim:-",remove.length);
+
+console.log("Trim:-",remove.trim().length);
+
+console.log("Trim:-",remove.trimStart().length);
+
+console.log("Trim:-",remove.trimEnd().length);
+
+// 1:-  Match method
+  //Match method is use for give us to full details and
+  //   //  also give index if is it no working then he give us to Null output.
+
+  let play="i am playing cricket";
+  console.log("Match method:-",play.match("play"));
+
+  
+// 2:-repeat
+//make a new string and copy strings
+
+const name="Mohin";
+console.log("Repeat :",Name.repeat(3));
+
+// 3:-replace
+// first match are replace
+// searching a new string a pattern and return a new string
+
+let latter="are you okay";
+console.log("Replace :",latter.replace("a","A"));
+
+// 4:-replace all
+// replace all work for replace all same latter 
+ word="this is book";
+ console.log("Replace All:-",word.replaceAll("o","O"));
+
+ //5:-search
+//  search a value and return a index position
+// search has a include a regular  expression 
+// index has a not a include a regular expression that the different between this two 
+
+let book="War and Peace";
+console.log("Search:-",book.search("a"));
+
+// 6:-slice 
+//abstract a part of string and return a new string
+
+let Animal="This is so many animal";
+console.log("Slice:-",Animal.slice(2));
+console.log("Slice:-",Animal.slice(6));
+console.log("Slice:-",Animal.slice(-1));
+
+// 7:-split 
+//firstly they convert string into array
+
+let mobile="This is my new mobile";
+console.log("Split:-",mobile.split(" "))   //with space
+console.log("Split:-",mobile.split(""))   //without space
+console.log("Split:-",mobile.split("i"))   //removing latter
+
+// 8:-start with
+// give us to output in boolean value 
+// if is latter in the string then output is true rather then output is false
+
+let text="hello everyone";
+
+console.log("Start With:-",text.startsWith("h"));
+
+// 9:-substring
+//substring does not change original array
+//
+
+let number="abcdefg";
+console.log("Substring:-",number.substring(2,5));
+console.log("Substring:-",number.substring(5,2));
+console.log("Substring:-",number.substring(-1));//give full text
+
+// 10:-uppercase
+//write touppercase()
+//if text has a lowercase we can change into uppercase
+
+let upper="good morning";
+console.log("Uppercase:-",upper.toUpperCase());
+
+// 11:-lowercase
+//write tolowercase()
+//if text has a uppercase wa can change into lowercase
+
+let lower="GOOD AFTERNOON";
+console.log("Lowercase:-",lower.toLowerCase());
+
+// 12:-trim 
+// use for remove white space
+
+let remove="  hello  everyone   ";
+
+console.log("Trim:-",remove.length);
+
+console.log("Trim:-",remove.trim().length);
+
+console.log("Trim:-",remove.trimStart().length);
+
+console.log("Trim:-",remove.trimEnd().length);
